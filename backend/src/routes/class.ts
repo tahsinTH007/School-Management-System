dd2a4af7d5fc59bf8c551457db372c4d03de557a
Tahsin Hassan <tahsin.hassan.700@gmail.com>
2026-05-17 22:55:57 +0600

    Backend: update class and delete class

--- backend/src/routes/class.ts
+++ backend/src/routes/class.ts
@@ -1,10 +1,17 @@
 import express from "express";
-import { createClass, getAllClasses } from "../controllers/class.ts";
+import {
+  createClass,
+  deleteClass,
+  getAllClasses,
+  updateClass,
+} from "../controllers/class.ts";
 import { authorize, protect } from "../middleware/auth.ts";
 
 const classRouter = express.Router();
 
 classRouter.post("/create", protect, authorize(["admin"]), createClass);
 classRouter.get("/", protect, authorize(["admin"]), getAllClasses);
+classRouter.patch("/update/:id", protect, authorize(["admin"]), updateClass);
+classRouter.delete("/delete/:id", protect, authorize(["admin"]), deleteClass);
 
 export default classRouter;
